b28f288e6e384ae18ac72f1a63724275386d7cb2
Manish <its.nathmanish@gmail.com>
2025-03-07 12:36:47 +0530

    Added functionality to the whole script.js file

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("connected");
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice(){
+    let num =  Math.random();
+    if(num < 0.34) return 'rock';
+    else if(num <= 0.67) return 'paper';
+    else return 'scissors';
+}
+function humanChoice(){
+    let choice = prompt('Enter your choice: rock, paper or scissors');
+    return choice.toLowerCase();
+}
+
+function playRound(computerChoice, humanChoice){
+    computerChoice = getComputerChoice();
+    humanChoice = humanChoice();
+    if(humanChoice === computerChoice) return 'It is a tie!';
+    if(humanChoice === 'rock'){
+        if(computerChoice === 'paper') return 'Computer wins!' && computerScore++;
+        else return 'Human wins!' && humanScore++;
+    }
+    if(humanChoice === 'paper'){
+        if(computerChoice === 'scissors') return 'Computer wins!' && computerScore++;
+        else return 'Human wins!' && humanScore++;
+    }
+    if(humanChoice === 'scissors'){
+        if(computerChoice === 'rock') return 'Computer wins!' && computerScore++;
+        else return 'Human wins!' && humanScore++;
+    }
+
+}
+function playGame(){
+    for(let i = 0; i < 5; i++){
+        console.log(playRound());
+    }
+    if(humanScore > computerScore) console.log('Human wins the game!');
+    else if(humanScore < computerScore) console.log('Computer wins the game!');
+    else console.log('It is a tie!');
+}
